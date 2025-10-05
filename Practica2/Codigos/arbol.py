4c3894821efa44a5ec5fd3481a2c0119312c010d
zaddkiel <zmartineza2000@alumno.ipn.mx>
2025-10-05 07:59:06 -0600

    modified arbol P1 v1.1

--- Practica2/Codigos/arbol.py
+++ Practica2/Codigos/arbol.py
@@ -23,35 +23,12 @@ class Arbol:
     def generar_vecinos(self, mapa, nodo_actual):
         x, y = nodo_actual.coordenada.x, nodo_actual.coordenada.y
         vecinos = [(x+1,y),(x-1,y),(x,y+1),(x,y-1)]
-
+        # Aqui hya q cambiar porq ya gerneramos lista de movimientos al actualizar el estado del agente
+        # Solo hay q trabajar con ella y se vaya generando
+        # Chequen el tema de generadores
         for posx, posy in vecinos:
             if 0 <= posx < mapa.ancho and 0 <= posy < mapa.alto:
                 coord = mapa.obtenerCoordenada(posx, posy)
                 if coord.avanzable and not coord.visitado:
                     hijo = self.agregar_hijo(nodo_actual, coord)
                     yield hijo
-
-    # def dfs(self, mapa, nodo_actual, meta):
-    #     coord = nodo_actual.coordenada
-    #     if (coord.x, coord.y) in self.visitados:
-    #         return None
-
-    #     self.visitados.add((coord.x, coord.y))
-    #     coord.visitado = True
-
-    #     if coord == meta:
-    #         return nodo_actual
-
-    #     for hijo in self.generar_vecinos(mapa, nodo_actual):
-    #         resultado = self.dfs(mapa, hijo, meta)
-    #         if resultado:
-    #             return resultado
-    #     return None
-
-    # def obtener_camino(self, nodo_final):
-    #     camino = []
-    #     actual = nodo_final
-    #     while actual:
-    #         camino.append((actual.coordenada.x, actual.coordenada.y))
-    #         actual = actual.padre
-    #     return list(reversed(camino))
